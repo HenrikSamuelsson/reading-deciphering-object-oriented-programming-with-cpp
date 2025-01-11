f81fd318ebc036a21603f13d8c7abd7f79345d2b
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-01-11 11:36:43 +0100

    Add my version of chapter 1 example 2

---
+++ chapter-01/examples/example-02/chapter-01-example-02.cpp
@@ -0,0 +1,41 @@
+// Example to illustrate usage of iostream manipulators.
+
+#include <iomanip>    // Manipulators.
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+// Places a newline charater in the cout buffer and flushes the buffer.
+using std::endl;
+
+// Clears the content of the output stream buffer.
+using std::flush;
+
+// Defines the precision used to print floating point numbers.
+using std::setprecision;
+
+// Sets the width for input and output.
+using std::setw;
+
+int main()
+{
+    const int buffSize = 20;
+    char name[buffSize];    // Caution, uninitialized array of char.
+    float gpa = 0.0;
+
+    cout << "Please enter a name: ";
+
+    // By using setw we won't overflow the input buffer of name.
+    cin >> setw(buffSize) >> name;
+    // Take out 1000 characters from the buffer but stop if finding a newline.
+    cin.ignore(1000, '\n');
+
+    cout << "Please enter a grade point average (GPA): ";
+    cin >> gpa;
+
+    cout << "Hello " << name << flush;
+    cout << ". GPA is: " << setprecision(3) << gpa << endl;
+
+    return 0;
+}
